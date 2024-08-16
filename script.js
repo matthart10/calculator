9b10658daf6107a8a00e7dc4104b16ea756d6f04
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-15 18:20:46 -0700

    Create operator functions and operation variables
    Create add, subtract, multiply, divide functions that take in two
    numbers. Then create variables that define a calulator operation.
    Two variables that are numbers and one variable that will be the
    operation.

---
+++ script.js
@@ -0,0 +1,23 @@
+// Functions that execute basic math operators
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+  };
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+// Variables that define a single calculator operation
+
+var firstNum = 0;
+var secondNum = 0;
+var operation = "";
