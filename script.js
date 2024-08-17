ae3d0d49b650e119336f57d98f72f0f11ec58915
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-17 00:19:44 -0700

    Begin frontend development/UI of calculator
    Create div boxes and flexbox to have a grey calculator, and have a
    white display on top, with buttons that go up to 9 using for loop
    in script.js. Add variable styles.

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ const divide = function(a, b) {
 var firstNum = 0;
 var secondNum = 0;
 var operation = "";
+
+// Operate function that takes in operator and two numbers and calls above function
+
+function operate() {
+
+}
+let grid = document.querySelector("#grid")
+for (let i=1; i < 10; i++) {
+  const cell = document.createElement("button");
+  cell.style.boxSizing = "border-box";
+  cell.style.width = 140 + "px";
+  cell.style.height = 140 + "px";
+  cell.textContent = `${i}`;
+  grid.appendChild(cell);
+}
