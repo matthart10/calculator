d870268437085ecbb0f6d9f5675b879908e47e89
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-08-31 08:49:24 -0700

    Add comments and clean up code for readability

--- script.js
+++ script.js
@@ -26,24 +26,28 @@ var secondNum = "";
 var operation = "";
 var clearOnNextNum = false;
 var equalPressed = false;
+var operatorPressedOnce = false;
 
 // Operate function that takes in operator and two numbers and calls above function
 
 function operate(a, b, operation) {
   if (operation === "+") {
-    return(add(a, b))
+    return(add(a, b));
   } else if (operation === "-") {
-    return (subtract(a, b))
+    return (subtract(a, b));
   } else if (operation === "×") {
-    return (multiply(a, b))
+    return (multiply(a, b));
   } else if (operation === "÷") {
-    return (divide(a, b))
+    return (divide(a, b));
   }
 }
-let grid = document.querySelector("#grid")
 
+// All non operation buttons section of calculator 
+
+let grid = document.querySelector("#grid");
 
 // Create plus/minus button
+
 const signButton = document.createElement("button");
 signButton.classList.add("signButton");
 signButton.classList.add("miscButton");
@@ -51,6 +55,7 @@ signButton.textContent = "+/-";
 grid.appendChild(signButton);
 
 // Create backspace button
+
 const backspaceButton = document.createElement("button");
 backspaceButton.classList.add("backspaceButton");
 backspaceButton.classList.add("miscButton");
@@ -58,13 +63,15 @@ backspaceButton.textContent = "⌫";
 grid.appendChild(backspaceButton);
 
 // Create clear button
+
 const clearButton = document.createElement("button");
 clearButton.classList.add("clearButton");
 clearButton.classList.add("miscButton");
 clearButton.textContent = "AC";
-grid.appendChild(clearButton)
+grid.appendChild(clearButton);
 
 // Create number buttons
+
 for (let i=9; i > 0; i--) {
   const cell = document.createElement("button");
   cell.classList.add("numberButton");
@@ -73,22 +80,26 @@ for (let i=9; i > 0; i--) {
 }
 
 // Add decimal button
+
 const decimalButton = document.createElement("button");
 decimalButton.classList.add("decimalButton");
 decimalButton.textContent = ".";
 grid.appendChild(decimalButton);
 
 // Create zero button
+
 const zeroButton = document.createElement("button");
 zeroButton.classList.add("numberButton");
 zeroButton.classList.add("zeroButton");
 zeroButton.textContent = "0";
 grid.appendChild(zeroButton);
 
+// Section of calculator (far right column) for operator buttons
 
+operatorGrid = document.querySelector("#operatorGrid");
 
- operatorGrid = document.querySelector("#operatorGrid")
 // Create operator buttons
+
 const addButton = document.createElement("button");
 addButton.classList.add("operatorButton");
 addButton.textContent = `+`;
@@ -112,54 +123,63 @@ operatorGrid.appendChild(divideButton);
 // Create equals button
 
 const equalButton = document.createElement("button");
-equalButton.classList.add("equalButton")
+equalButton.classList.add("equalButton");
 equalButton.textContent = `=`;
 operatorGrid.appendChild(equalButton);
 
-// Event Listener that shows if a number button is pressed then put it up on the display
+// Event Listener for number buttons, call numberButtonsAndKeyboard on click
 
-const theDisplay = document.querySelector("#display")
+const theDisplay = document.querySelector("#display");
 
-const allNumButtons = document.querySelectorAll(".numberButton")
+const allNumButtons = document.querySelectorAll(".numberButton");
 allNumButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    numberButtonsAndKeyboard()
+    numberButtonsAndKeyboard();
+    // Don't exceed the display width
     if (theDisplay.textContent.length < 16) {
       theDisplay.textContent += `${button.textContent}`;
-    }
-  })
-})
-var operatorPressedOnce = false;
+    };
+  });
+});
+
 // Event Listener that once an operation is pressed, then store the text content inside a variable and store operation so we know what operation to do once equals happens
+
 const allOperatorButtons = document.querySelectorAll(".operatorButton");
 allOperatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-      // operatorPressedOnce should have returned to false when equals was pressed, but this will trigger if we're doing multi-operative operation
-      if (operatorPressedOnce) {
-        secondNum = theDisplay.textContent;
-        firstNum = Number(`${firstNum}`);
-        secondNum = Number(`${secondNum}`);
-        if (operate(firstNum, secondNum, operation).toString().length < 17) {
-          theDisplay.textContent = `${operate(firstNum, secondNum, operation)}`;
-        } else {
-          theDisplay.textContent = `${(operate(firstNum, secondNum, operation)).toPrecision(2)}`;
-        }
-      }
-      clearOnNextNum = true;
-      equalPressed = false;
-      operatorPressedOnce = true;
-      firstNum = theDisplay.textContent;
-      operation = `${button.textContent}`
-    });
+    // operatorPressedOnce should have returned to false when equals was pressed, but this will trigger if we're doing multi-operative operation
+    if (operatorPressedOnce) {
+      secondNum = theDisplay.textContent;
+      firstNum = Number(`${firstNum}`);
+      secondNum = Number(`${secondNum}`);
+      // If the answer will overflow the display, shorten answer to two sig figs
+      if (operate(firstNum, secondNum, operation).toString().length < 17) {
+        theDisplay.textContent = `${operate(firstNum, secondNum, operation)}`;
+      } else {
+        theDisplay.textContent = `${(operate(firstNum, secondNum, operation)).toPrecision(2)}`;
+      };
+    };
+    // Check to change to second number after this press
+    clearOnNextNum = true;
+    // Check to allow operation to continue if wanted after an equal press
+    equalPressed = false;
+    // Check to allow multi-operative operations
+    operatorPressedOnce = true;
+    // Set number and operation on clicks
+    firstNum = theDisplay.textContent;
+    operation = `${button.textContent}`
   });
+});
 
-// Event Listener that once an equal is pressed, then the store the text content inside the second number and evaluate the two numbers
+// Event Listener for equal button
+
 const theEqualButton = document.querySelector(".equalButton");
 theEqualButton.addEventListener("click", () => {
   equalButtonAndKeyboard();
 });
 
-// Event Listener that once clear is pressed, then wipe out all numbers and display is blank;
+// Event Listener that once clear is clicked, reset everything
+
 const theClearButton = document.querySelector(".clearButton");
 theClearButton.addEventListener("click", () => {
   theDisplay.textContent = "0";
@@ -171,28 +191,34 @@ theClearButton.addEventListener("click", () => {
   operatorPressedOnce = false;
 });
 
-// Event Listener that once backspace button is pressed, execute backspace function
+// Event Listener for backspace button
+
 const theBackspaceButton = document.querySelector(".backspaceButton");
 theBackspaceButton.addEventListener("click", () => {
   backspaceButtonAndKeyboard();
 });
 
 // Event Listener that once sign button is pressed, multiply textcontent by negative 1
+
 const theSignButton = document.querySelector(".signButton");
 theSignButton.addEventListener("click", () => {
   theDisplay.textContent = Number(`${theDisplay.textContent}`) * -1;
 });
 
-// Event Listener that once decimal button is pressed, add a decimal
+// Event Listener for decimal button
+
 const theDecimalButton = document.querySelector(".decimalButton");
 theDecimalButton.addEventListener("click", () => {
   decimalButtonAndKeyboard();
 });
 
-// Keyboard Support for numbers, decimal, and enter/equals
+// Keyboard Support
+
 document.addEventListener("keydown", (e) => {
+  // If keydown was a number
   if (isFinite(e.key)) {
     numberButtonsAndKeyboard();
+    // Overflow check
     if (theDisplay.textContent.length < 16) {
       theDisplay.textContent += `${e.key}`;
     }
@@ -211,7 +237,7 @@ document.addEventListener("keydown", (e) => {
 // Functions
 
 function numberButtonsAndKeyboard() {
-  // clearOnNextNum is set to true after pressing an operator button so new number can be read, also when equals is pressed so new calculator operation can be started
+  // clearOnNextNum is set to true after pressing an operator button so new number can be read
   if (clearOnNextNum) {
     theDisplay.textContent = "";
     clearOnNextNum = false;
@@ -244,15 +270,15 @@ function backspaceButtonAndKeyboard() {
     // Once all the numbers in the display are backspaced, return the display to zero
     if (theDisplay.textContent === "") {
       theDisplay.textContent = "0";
-    }
-  }
-}
+    };
+  };
+};
 
 function decimalButtonAndKeyboard() {
   if (clearOnNextNum) {
     theDisplay.textContent = "0.";
     clearOnNextNum = false;
-    // Do nothing if there is a decimal in the current display
+  // Do nothing if there is a decimal in the current display
   } else if (theDisplay.textContent.indexOf('.') > -1) {
   } else {
     theDisplay.textContent += "."
@@ -260,22 +286,23 @@ function decimalButtonAndKeyboard() {
 }
 
 function equalButtonAndKeyboard() {
-  // As long as we have an operation and a number, we have the display be second number and we execute an operation
+  // As long as we have an operation and a number and equal was set back to false, we have the display be second number and we execute an operation
   if (firstNum != "" && operation != "" && !equalPressed) {
-      operatorPressedOnce = false;
-      secondNum = theDisplay.textContent;
-      firstNum = Number(`${firstNum}`);
-      secondNum = Number(`${secondNum}`);
-      if (operate(firstNum, secondNum, operation).toString().length < 17) {
-        theDisplay.textContent = `${operate(firstNum, secondNum, operation)}`;
-      } else {
-        theDisplay.textContent = `${(operate(firstNum, secondNum, operation)).toPrecision(2)}`;
-      }
-      equalPressed = true;
-      clearOnNextNum = true;
-      operation = "";
-    }
-  }
+    operatorPressedOnce = false;
+    secondNum = theDisplay.textContent;
+    firstNum = Number(`${firstNum}`);
+    secondNum = Number(`${secondNum}`);
+    // Overflow check
+    if (operate(firstNum, secondNum, operation).toString().length < 17) {
+      theDisplay.textContent = `${operate(firstNum, secondNum, operation)}`;
+    } else {
+      theDisplay.textContent = `${(operate(firstNum, secondNum, operation)).toPrecision(2)}`;
+    };
+    equalPressed = true;
+    clearOnNextNum = true;
+    operation = "";
+  };
+};
 
 // Event Listener for all operator buttons that if it is actively clicked, then have hover background, if not, then return to yellowgreen background
 const allButtons = document.querySelectorAll("button");
